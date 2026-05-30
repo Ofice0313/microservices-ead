777e7add8a19ff0303403a33a2e39efc4dcd8c44
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-05-31 00:04:19 +0200

    Microservices created and database added

---
+++ authUser/src/test/java/com/devcaleb/ead/authUser/AuthUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcaleb.ead.authUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification/src/main/java/com/devcaleb/ead/notification/NotificationApplication.java
@@ -0,0 +1,13 @@
+package com.devcaleb.ead.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
