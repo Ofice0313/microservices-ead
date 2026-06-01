874a99bcfd0e4319157b17e6cc29246ac7651282
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-06-01 13:20:02 +0200

    Validations 001

--- authUser/src/main/java/com/devcaleb/ead/authUser/controllers/UserController.java
+++ authUser/src/main/java/com/devcaleb/ead/authUser/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -52,7 +53,8 @@ public class UserController {
     @PutMapping("/{userId}")
     public ResponseEntity<Object> updateUser(
             @PathVariable(value = "userId") UUID userId,
-            @RequestBody @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO) {
+            @RequestBody @Validated(UserDTO.UserView.UserPut.class)
+            @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO) {
         Optional<User> userOptional = service.findById(userId);
         if(!userOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
@@ -70,7 +72,8 @@ public class UserController {
     @PutMapping("/{userId}/password")
     public ResponseEntity<Object> updatePassword(
             @PathVariable(value = "userId") UUID userId,
-            @RequestBody @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO userDTO) {
+            @RequestBody @Validated(UserDTO.UserView.PasswordPut.class)
+            @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO userDTO) {
         Optional<User> userOptional = service.findById(userId);
         if(!userOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
@@ -88,7 +91,8 @@ public class UserController {
     @PutMapping("/{userId}/image")
     public ResponseEntity<Object> updateImage(
             @PathVariable(value = "userId") UUID userId,
-            @RequestBody @JsonView(UserDTO.UserView.ImagePut.class) UserDTO userDTO) {
+            @RequestBody @Validated(UserDTO.UserView.ImagePut.class)
+            @JsonView(UserDTO.UserView.ImagePut.class) UserDTO userDTO) {
         Optional<User> userOptional = service.findById(userId);
         if(!userOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
