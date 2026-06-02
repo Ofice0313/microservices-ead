0cf8459ecd6f781823aa186c5013776450154385
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-06-02 11:29:01 +0200

    Relacionamentos One-to-Many and Many-to-One, Implementação

--- course/src/main/java/com/devcaleb/ead/course/entities/Module.java
+++ course/src/main/java/com/devcaleb/ead/course/entities/Module.java
@@ -2,11 +2,13 @@ package com.devcaleb.ead.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -28,5 +30,12 @@ public class Module implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false)
+    private Course course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "module")
+    private Set<Lesson> lessons;
 
 }
